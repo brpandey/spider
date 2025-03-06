1842b1ae97cc279a1a0fd71768b8b673e08bc849
Bibek Pandey <bibekp@gmail.com>
2025-03-05 16:38:45 -0800

    Small tweaks

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -8,22 +8,11 @@ config.exit_behavior = "Close"
 
 config.color_scheme = "Galaxy"
 -- config.color_scheme = "Nova (base16)"
--- config.colors = {
--- 	foreground = "#CBE0F0",
--- 	background = "#011423",
--- 	--  cursor_bg = "#47FF9C",
--- 	--	cursor_border = "#47FF9C",
--- 	cursor_bg = "white",
--- 	cursor_border = "white",
--- 	cursor_fg = "#011423",
--- 	selection_bg = "#033259",
--- 	selection_fg = "#CBE0F0",
--- 	ansi = { "#214969", "#E52E2E", "#44FFB1", "#FFE073", "#0FC5ED", "#a277ff", "#24EAF7", "#24EAF7" },
--- 	brights = { "#214969", "#E52E2E", "#44FFB1", "#FFE073", "#A277FF", "#a277ff", "#24EAF7", "#24EAF7" },
--- }
+-- config.font = wezterm.font("Fantasque Sans Mono")
 
 config.font = wezterm.font("MesloLGS NF")
 config.font_size = 11
+
 config.line_height = 1.2
 
 config.hide_tab_bar_if_only_one_tab = true
@@ -36,6 +25,14 @@ config.window_padding = {
 config.window_decorations = "RESIZE"
 config.window_background_opacity = 0.9
 config.macos_window_background_blur = 10
+
+config.keys = {
+	{
+		key = "n",
+		mods = "SHIFT|CTRL",
+		action = wezterm.action.ToggleFullScreen,
+	},
+}
 
 -- and finally, return the configuration to wezterm
 return config
