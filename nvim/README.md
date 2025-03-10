c9a9c016b1194f52b7f9182ca7294ec8317693ce
Bibek Pandey <bibekp@gmail.com>
2025-03-10 13:24:12 -0700

    Added nvim setup script

--- nvim/README.md
+++ nvim/README.md
@@ -4,3 +4,11 @@
 
 sudo apt install npm
 sudo apt install python3-venv
+
+or 
+
+use local setup.sh script to install and stow!
+
+```bash
+$ setup.sh
+```
---
+++ nvim/setup.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+MODULE_PATH=$(dirname "$(realpath "$0")")
+MODULE_NAME=$(basename $MODULE_PATH)
+SPIDER_PATH=$(dirname $MODULE_PATH)
+
+# Pull in shared top-level functions and variables
+source "$SPIDER_PATH/shared.sh"
+
+print_colored "$GREEN" "Setting up $MODULE_NAME (spider) module"
+
+CMD=nvim
+NVIM_TARGET_DIR="/opt"
+ADDITIONAL="npm python3-venv"
+
+install_neovim() {
+    if ! command_exists $CMD; then
+        ${SUDO_CMD} ${PACK_MGR} install -yq $ADDITIONAL
+
+        if ! curl -L https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.tar.gz | $SUDO_CMD tar -xz -C $NVIM_TARGET_DIR; then
+            print_colored "$RED" "Something went wrong during $CMD install!"
+            exit 1
+        fi
+    else
+        printf "$CMD already installed\n"
+    fi
+}
+
+install_neovim
+apply_stow $SPIDER_PATH $MODULE_NAME
