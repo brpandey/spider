787b0320b586ad14b3048fbaea25bc5ce8d4b3cd
Bibek Pandey <bibekp@gmail.com>
2025-03-10 16:14:28 -0700

    Added wezterm setup script

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -1,18 +1,14 @@
--- Pull in the wezterm API
+-- Load wezterm API
 local wezterm = require("wezterm")
-
--- This will hold the configuration.
 local config = wezterm.config_builder()
 
 config.exit_behavior = "Close"
-
 config.color_scheme = "Galaxy"
 -- config.color_scheme = "Nova (base16)"
--- config.font = wezterm.font("Fantasque Sans Mono")
+config.font = wezterm.font("Fantasque Sans Mono")
+-- config.font = wezterm.font("MesloLGS NF")
 
-config.font = wezterm.font("MesloLGS NF")
-config.font_size = 11
-
+config.font_size = 13
 config.line_height = 1.2
 
 config.hide_tab_bar_if_only_one_tab = true
@@ -26,13 +22,12 @@ config.window_decorations = "RESIZE"
 config.window_background_opacity = 0.9
 config.macos_window_background_blur = 10
 
-config.keys = {
-	{
-		key = "n",
-		mods = "SHIFT|CTRL",
-		action = wezterm.action.ToggleFullScreen,
-	},
-}
+-- config.keys = {
+-- 	{
+-- 		key = "n",
+-- 		mods = "SHIFT|CTRL",
+-- 		action = wezterm.action.ToggleFullScreen,
+-- 	},
+-- }
 
--- and finally, return the configuration to wezterm
 return config
