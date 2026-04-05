cf5363686fc5c8e7e4a8b6d64d1ae590f2c004f2
Bibek Pandey <bibekp@gmail.com>
2026-04-05 16:05:00 -0700

    Small omission fixed

--- cli-tools/setup.sh
+++ cli-tools/setup.sh
@@ -7,7 +7,7 @@ greeting $0
 # Include additional packages for use in the command line
 CLI_TOOLS=(gawk stow git curl wget make bat trash-cli eza fzf \ 
     ripgrep gedit okular ranger htop gnome-software gnome-tweaks
-    libfuse2t64 krop gparted stacer okular imagemagick) # tldr
+    libfuse2t64 krop gparted stacer okular imagemagick pdftk) # tldr
 
 install_cli_tools() {
     echo "About to install CLI Tools"
