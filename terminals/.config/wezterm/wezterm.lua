33c24bdcbed9150bd36ff046855d936c9bc01b51
Bibek Pandey <bibekp@gmail.com>
2026-04-06 22:33:52 -0700

    Fixed wezterm config issues for wayland

--- terminals/.config/wezterm/wezterm.lua
+++ terminals/.config/wezterm/wezterm.lua
@@ -1,32 +1,28 @@
--- Load wezterm API
 local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
 config.exit_behavior = "Close"
 config.color_scheme = "Galaxy"
--- config.color_scheme = "Nova (base16)"
 config.font = wezterm.font("Fantasque Sans Mono")
--- config.font = wezterm.font("MesloLGS NF")
-
 config.font_size = 13
-config.line_height = 1.2
 
-config.hide_tab_bar_if_only_one_tab = false
+-- safer than 1.2 on Wayland
+config.line_height = 1.0
+
+-- keep padding EVEN numbers (important!)
 config.window_padding = {
 	left = 4,
-	right = 2,
-	top = "0px",
-	bottom = "0px",
+	right = 4,
+	top = 2,
+	bottom = 2,
 }
-config.window_decorations = "RESIZE"
+
+-- safest on Wayland
+config.window_decorations = "NONE"
+
+-- you can try turning this back on later
 config.window_background_opacity = 0.7
 
--- config.keys = {
--- 	{
--- 		key = "n",
--- 		mods = "SHIFT|CTRL",
--- 		action = wezterm.action.ToggleFullScreen,
--- 	},
--- }
+config.hide_tab_bar_if_only_one_tab = false
 
 return config
